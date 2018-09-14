b0d376855eb2878134254af5a0faf5b22a992abd
sdorgan <sebastien.dorgan@c-s.fr>
2018-09-14 19:29:42 +0200

    Add And, Or and Xor predicate composer

--- talgo.go
+++ talgo.go
@@ -12,10 +12,31 @@ type Visitor func(i int)
 //Predicate defines a predicate that applies to the ith elements of a collection
 type Predicate func(i int) bool
 
-//Negate negates a predictae
+//Negate negates a predicate
 func Negate(p Predicate) Predicate {
 	return func(i int) bool {
 		return !p(i)
+	}
+}
+
+//Or create a new predicte that is the 'or' of predicate p1 and p2
+func Or(p1 Predicate, p2 Predicate) Predicate {
+	return func(i int) bool {
+		return p1(i) || p2(i)
+	}
+}
+
+//And create a new predicte that is the 'and' of predicate p1 and p2
+func And(p1 Predicate, p2 Predicate) Predicate {
+	return func(i int) bool {
+		return p1(i) && p2(i)
+	}
+}
+
+//Xor create a new predicte that is the 'xor' of predicate p1 and p2
+func Xor(p1 Predicate, p2 Predicate) Predicate {
+	return func(i int) bool {
+		return p1(i) != p2(i)
 	}
 }
 
